b1043dde7f38938195fd2fb05731e800067c7759
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-09-25 14:40:52 -0500

    Libreria de administracion del cliente

---
+++ cliente/cliente.h
@@ -0,0 +1,19 @@
+#include "../carrito/carrito.h"
+
+#define CLIENT_ID_SIZE 20
+#define CLIENT_NAME_MAX_SIZE 30
+#define HISTORY_SIZE 100
+#define PASSWORD_SIZE 20
+
+typedef struct CLIENTE {
+  Carrito carro;
+  char id[CLIENT_ID_SIZE];
+  char password[PASSWORD_SIZE];
+  char name[CLIENT_NAME_MAX_SIZE];
+  float historial_compras[HISTORY_SIZE];
+} Cliente;
+
+// Operaciones de administración del cliente
+Cliente nuevo_cliente();
+Cliente carga_cliente();
+void guarda_cliente(Cliente);
