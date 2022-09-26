99b37809f1a5c327eed23e94eb52c377fa77a348
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-09-26 18:18:26 -0500

    Reenombramiento de archivos

--- leer_archivo/lee_archivo.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-File *abrir_archivo(char *ruta, char *modo) {
-  File *archivo = fopen(ruta, modo);
-  if (archivo == NULL)
-    printf("No se puede abrir el archivo\n")
-  return archivo;
-}
