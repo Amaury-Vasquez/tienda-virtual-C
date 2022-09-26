6bd1a43581bfaddf239f29211828b2071bf2ffff
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-09-26 18:16:11 -0500

    Probada la funcion de leer archivo para crear carrito

--- main.c
+++ main.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
+
 #include "carrito/carrito.h"
 
 int main() {
-  Carrito carro = nuevo_carro();
-  Producto productos[5];
-  int i;
-  for (i = 0; i < 5; i++) {  
-    productos[i] = leer_producto();
-    add_al_carro(&carro, productos[i], i + 1);
-  }
-  imprimir_carro(carro);
-  eliminar_del_carro(&carro, productos[4].id, 2);
-  eliminar_del_carro(&carro, productos[2].id, 4);
-  guardar_carro(carro, "./carrito/datos/2021630014.txt");
+  Carrito carro = leer_carro("./carrito/datos/2021630014.txt");
   imprimir_carro(carro);
   borrar_carro(&carro);
   return 0;
