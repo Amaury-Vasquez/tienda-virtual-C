0d00c82f0c083e06fac7db8055edfba49c8fd10f
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-01-11 15:22:36 -0600

    servidor

--- proceso_servidor.c
+++ proceso_servidor.c
@@ -3,7 +3,16 @@
 #include "memoria/memoria.h"
 #include "semaforo/semaforo.h"
 
+volatile sig_atomic_t termina = 0;
+
+void term(int sigum);
+
 int main() {
+  struct sigaction action;
+  memset(&action, 0, sizeof(struct sigaction));
+  action.sa_handler = term;
+  sigaction(SIGTERM, &action, NULL);
+
   int region_catalogo = crear_region_catalogo();
   Catalogo *catalogo = obtener_catalogo_compartido(region_catalogo);
   *catalogo = leer_catalogo("catalogo/catalogo.txt");
@@ -13,7 +22,12 @@ int main() {
   int region_carrito = crear_region_carrito();
   Rutas_carrito *rutas_carrito = obtener_carrito_compartido(region_carrito);
   clientes_compartidos->num_clientes = carga_nombre_clientes("./cliente/clientes.txt", rutas_carrito->rutas);
-  sleep(50);
+  int semaforo_servidor = obtener_semaforo_servidor();
+  while (1) {
+    printf("Esperando peticiones...");
+    down(semaforo_servidor);
+    guardar_catalogo(*catalogo, "catalogo/catalogo.txt");
+  }
   return 0;
 }
   // for(int i = 0; i < clientes_compartidos->num_clientes; i++) {
@@ -24,3 +38,8 @@ int main() {
   //     fclose(archivo);
   //   }
   // }
+
+void term(int sigum) {
+  printf("\nTerminando proceso...\n");
+  termina = 1;
+}
